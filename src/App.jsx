fe25d370165b67440cfbc49dbc04a5db734b3df2
hanzala bawany <hanzalabawany0@gmail.com>
2026-01-19 15:11:10 +0500

    .

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import Home from './components/Home'
+import { Route, Routes } from 'react-router-dom'
+
+function App() {
+ 
+
+  return (
+    <>
+    <Routes>
+
+      <Route path="/home" element={<Home />} />
+
+    </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+
+const Home = () => {
+
+  return (
+    <div className="bg-amber-400  h-full w-full">Home</div>
+  )
+}
+
+export default Home
