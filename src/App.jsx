4a1378bb373ad35564c44c15329e0325c4805562
GoharRamzan <hanzalabawany0@gmail.com>
2026-01-20 16:25:09 +0500

    first animation by me

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <>
     <Routes>
 
-      <Route path="/home" element={<Home />} />
+      <Route path="/firstClass" element={<Home />} />
 
     </Routes>
     </>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,36 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useRef } from "react";
 
 const Home = () => {
 
+  const box = useRef(null)
+  const boxParent = useRef(null)
+
+  const animation = {
+    x: 200,
+    duration: 2,
+    delay: 0.5,
+  }
+
+  const animation2 = {
+    delay: 0.5,
+    duration: 5,
+    x: 800,
+    rotate : 720
+  }
+
+  useGSAP(() => {
+    // gsap.to(".box", animation)  // ye bhi kar sakte hen
+    gsap.to(box.current, animation2)  
+  })
+  // , { scope: boxParent } }) agar ye kar dun to ab sirf boxParent ke andar hi animation apply ho gi chae box classs ke kitne hi alement ho ... ziada tar class ke sath use ho ta he
+
   return (
-    <div className="bg-amber-400  h-full w-full">Home</div>
+    <div ref={boxParent} className="h-[100vh] w-full bg-blue-100 flex overflow-hidden  items-center">
+      <div ref={box} className="box  bg-amber-400  h-[40px] w-[30%]">Home</div>
+    </div>
   )
 }
 
-export default Home
+export default Home;
